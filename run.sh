44b8f2907de3d08af0e051417c431268f08a0f58
GlacierSG <jokullsnr@gmail.com>
2021-12-22 02:24:54 +0000

    changed version in run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker container stop icesum
-docker container rm icesum
-docker build . -t icesum:example
-docker run -d --name=icesum -p 8080:8080 icesum:example
+docker container stop glaciersg/icesum_api:v1.0
+docker container rm glaciersg/icesum_api:v1.0
+docker build . -t glaciersg/icesum_api:v1.0
+docker run -d --name=icesum -p 8080:8080 glaciersg/icesum_api:v1.0
